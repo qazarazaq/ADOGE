1af9d4a38d78e54579b4576a940137288b6a738a
qazarazaq <54495457+qazarazaq@users.noreply.github.com>
2021-12-30 15:53:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 var img = document.getElementById("popcat1");
 var count = document.getElementById("score");
 var score = 0;
-var audio = new Audio('asset/slap.mp3');
+var audio = new Audio('slap.mp3');
 
 // mouseclick event
 img.addEventListener("mousedown", function () {
     increaseScore();
-    img.src = 'asset/popcat2.png';
+    img.src = 'popcat2.png';
     audio.play();
 });
 
 img.addEventListener("mouseup", function () {
-    img.src = 'asset/popcat1.png';
+    img.src = 'popcat1.png';
     audio.play();
 });
 
 // touch event
 img.addEventListener("touchstart", function () {
     increaseScore();
-    img.src = 'asset/popcat2.png';
+    img.src = 'popcat2.png';
     audio.play();
 });
 
 img.addEventListener("touchmove", function () {
-    img.src = 'asset/popcat1.png';
+    img.src = 'popcat1.png';
     audio.play();
 });
 
